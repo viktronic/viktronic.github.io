c0fea1beedab71a49854a6eb038a748e917550d8
viktronic <pvictor3@hotmail.es>
2016-03-02 10:37:36 -0600

    Update composicion.formas.js

--- rv/composicion.formas.js
+++ rv/composicion.formas.js
@@ -32,4 +32,11 @@ function setup(){
 function loop(){
   
   requestAnimationFrame( loop );
+  malla.rotation.x += 0.01;
+  malla.rotation.y += 0.01;
+  renderer.render( escena, camara );
 }
+
+var escena, camara, renderer, malla;
+setup();
+loop();
