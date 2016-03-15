82e2fb7afc51f46055d2f8607a8ca91512375eea
viktronic <pvictor3@hotmail.es>
2016-03-14 21:00:35 -0600

    Create robot.1.js

---
+++ rv/robot.1.js
@@ -0,0 +1,33 @@
+function setup(){
+  //var cuboForma = new THREE.BoxGeometry(2,2,2);
+  var ruedaForma= new THREE.Shape();
+  ruedaForma.moveto( 10, 10 );
+  ruedaForma.lineTo( 12, 10.5 );
+  ruedaForma.lineTo( 12, 11.5 );
+  ruedaForma.lineTo( 11, 10.5 );
+  
+  var ruedaGeometry = new THREE.ExtrudeGeometry( ruedaForma, {amount: 10});
+  ruedaMalla = new THREE.Mesh( ruedaGeometry, new THREE.MeshBasicMaterial() );
+  
+  escena = new THREE.Scene();
+  escena.add( ruedaMalla );
+  
+  camara = new THREE.PerspectiveCamera();
+  camara.position.z = 5;
+  
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerHeight*.95,window.innerHeight*.95);
+  document.body.appendChild( renderer.domElement );
+}
+
+function loop(){
+  requestAnimationFrame( loop );
+  ruedaMalla.position.x += 0.01;
+  ruedaMalla.position.y += 0.01;
+  
+  renderer.render( escena, camara );
+}
+
+var ruedaMalla, escena, camara, renderer;
+setup();
+loop();
