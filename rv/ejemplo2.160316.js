c2a47c25b2aa29da60507367f77896b874881b0f
viktronic <pvictor3@hotmail.es>
2016-03-16 10:34:46 -0600

    Create ejemplo2.160316.js

---
+++ rv/ejemplo2.160316.js
@@ -0,0 +1,35 @@
+function setup(){
+THREE.ImageUtils.crossOrigin = '';
+var textura = THREE.ImageUtils.loadTexture('http://threejs.org/examples/texture/brick_difusse.jpg')
+  var forma = new THREE.BoxGeometry( 1, 4, 9 );
+  var material = new THREE.MeshBasicMaterial( {map: textura} );
+  malla = new THREE.Mesh( forma, material );
+  
+  var luzPuntual = new THREE.PointLight(0xFFFFFF);
+  luzPuntual.position.x = 10;
+  luzPuntual.position,y = 10;
+  luzPuntual.position.z = 10;
+  
+  escena = new THREE.Scene();
+  escena.add(malla);
+  escena.add(luzPuntual);
+  
+  camara = new THREE.PerspectiveCamera();
+  camara.position.z = 5;
+  
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize( window.innerHeight*.95, window.innerHeight*.95);
+  document.body.appendChild( renderer.domElement );
+}
+
+function loop(){
+  requestAnimationFrame( loop );
+  malla.rotation.y += 0.01;
+  malla.rotation.x += 0.01;
+  
+  renderer.render( escena, camara );
+} 
+
+var renderer, camara, escena, malla;
+setup();
+loop();
