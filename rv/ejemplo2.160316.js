9d3d085b6d16124ad9f973451166f8a942cce9fa
viktronic <pvictor3@hotmail.es>
2016-03-16 10:45:09 -0600

    Update ejemplo2.160316.js

--- rv/ejemplo2.160316.js
+++ rv/ejemplo2.160316.js
@@ -1,6 +1,6 @@
 function setup(){
 THREE.ImageUtils.crossOrigin = '';
-var textura = THREE.ImageUtils.loadTexture('http://threejs.org/examples/texture/brick_difusse.jpg')
+var textura = THREE.ImageUtils.loadTexture('http://threejs.org/examples/textures/brick_diffuse.jpg');
   var forma = new THREE.BoxGeometry( 1, 4, 9 );
   var material = new THREE.MeshBasicMaterial( {map: textura} );
   malla = new THREE.Mesh( forma, material );
