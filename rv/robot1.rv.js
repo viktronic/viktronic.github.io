83c6fe9bdff7a05072d22fc701430d177e449ca7
viktronic <pvictor3@hotmail.es>
2016-04-07 21:45:40 -0500

    Update robot1.rv.js

--- rv/robot1.rv.js
+++ rv/robot1.rv.js
@@ -1,51 +1,33 @@
-function rueda(){
-	THREE.Object3D.call(this);
-	THREE.ImageUtils.crossOrigin = '';
-	this.textura = THREE.ImageUtils.loadTexture('http://threejs.org/examples/textures/brick_diffuse.jpg');
-	this.arcShape = new THREE.Shape();
-				this.arcShape.moveTo( 50, 10 );
-				this.arcShape.absarc( 10, 10, 40, 0, Math.PI*2, false );
-	this.holePath = new THREE.Path();
-				this.holePath.moveTo( 20, 10 );
-				this.holePath.absarc( 10, 10, 10, 0, Math.PI*2, true );
-				this.arcShape.holes.push( this.holePath );
-
-	this.extrudeSettings = { amount: 8, bevelEnabled: true, bevelSegments: 2, steps: 2, bevelSize: 1, bevelThickness: 1 };
-
-	this.rueda = new THREE.ExtrudeGeometry( this.arcShape, this.extrudeSettings );
-	this.material = new THREE.MeshPhongMaterial({ map: this.textura});
-	this.mallaRueda = new THREE.Mesh( this.rueda, this.material );
-
-	this.add(this.mallaRueda);
-}
-rueda.prototype = new THREE.Object3D();
-
-function visor(){
-	THREE.Object3D.call(this);
-	this.malla = new THREE.Mesh( new THREE.BoxGeometry( 20, 10, 30 ), new THREE.MeshPhongMaterial({ color: 0x0000ff }) );
-
-	this.add(this.malla);
-}
-visor.prototype = new THREE.Object3D();
-
 function setup(){
-	var material = new THREE.MeshPhongMaterial({color: 0xff0000 });
 
+	var arcShape = new THREE.Shape();
+				arcShape.moveTo( 50, 10 );
+				arcShape.absarc( 10, 10, 40, 0, Math.PI*2, false );
+	var holePath = new THREE.Path();
+				holePath.moveTo( 20, 10 );
+				holePath.absarc( 10, 10, 10, 0, Math.PI*2, true );
+				arcShape.holes.push( holePath );
+
+	var extrudeSettings = { amount: 8, bevelEnabled: true, bevelSegments: 2, steps: 2, bevelSize: 1, bevelThickness: 1 };
+
+	//var forma = new THREE.RingGeometry( 0.3, 2, 32 );
+	var rueda = new THREE.ExtrudeGeometry( arcShape, extrudeSettings );
 	var union = new THREE.CylinderGeometry( 10, 10, 150 );
 	var caja = new THREE.BoxGeometry( 200, 30, 130 );
 	var tubo = new THREE.CylinderGeometry( 10, 10, 100 );
-	//var visor = new THREE.BoxGeometry( 20, 10, 30 );
+	var visor = new THREE.BoxGeometry( 20, 10, 30 );
 
+	var material = new THREE.MeshNormalMaterial();
+
+	var mallaRueda1 = new THREE.Mesh( rueda, material );
+	var mallaRueda2 = new THREE.Mesh( rueda, material );
+	var mallaRueda3 = new THREE.Mesh( rueda, material );
+	var mallaRueda4 = new THREE.Mesh( rueda, material );
 	var mallaUnion1 = new THREE.Mesh( union, material );
 	var mallaUnion2 = new THREE.Mesh( union, material );
 	var mallaCaja = new THREE.Mesh( caja, material );
 	var mallaTubo = new THREE.Mesh( tubo, material );
-	mallaVisor = new visor();
-
-	mallaRueda1 = new rueda();
-	mallaRueda2 = new rueda();
-	mallaRueda3 = new rueda();
-	mallaRueda4 = new rueda();
+	var mallaVisor = new THREE.Mesh( visor, material );
 
 	mallaRueda1.position.set( 0, 0, 0);
 	mallaRueda2.position.set( 150, 0, 0);
@@ -59,8 +41,13 @@ function setup(){
 	mallaTubo.position.set( 0,90,80);
 	mallaVisor.position.set(0,145,80);
 
+
 	var robot = new THREE.Geometry();
 
+	THREE.GeometryUtils.merge( robot, mallaRueda1 );
+	THREE.GeometryUtils.merge( robot, mallaRueda2 );
+	THREE.GeometryUtils.merge( robot, mallaRueda3 );
+	THREE.GeometryUtils.merge( robot, mallaRueda4 );
 	THREE.GeometryUtils.merge( robot, mallaUnion1 );
 	THREE.GeometryUtils.merge( robot, mallaUnion2 );
 	THREE.GeometryUtils.merge( robot, mallaCaja );
@@ -69,23 +56,11 @@ function setup(){
 
 	mallaRobot = new THREE.Mesh( robot , material );
 
-	var luzPuntual = new THREE.PointLight( 0xffffff );
-  	luzPuntual.position.x = 50;
-  	luzPuntual.position.y = 100;
-  	luzPuntual.position.z = 500;
+	camara = new THREE.PerspectiveCamera();
+	camara.position.z = 500;
 
 	escena = new THREE.Scene();
 	escena.add( mallaRobot );
-	escena.add( mallaRueda1 );
-	escena.add( mallaRueda2 );
-	escena.add( mallaRueda3 );
-	escena.add( mallaRueda4 );
-	escena.add( mallaVisor );
-
-	escena.add(luzPuntual);
-
-	camara = new THREE.PerspectiveCamera();
-	camara.position.z = 500;
 
 	renderer = new THREE.WebGLRenderer();
 	renderer.setSize( window.innerHeight*.95, window.innerHeight*.95 );
@@ -95,11 +70,13 @@ function setup(){
 function loop(){
 	requestAnimationFrame( loop );
 
+	//mallaRobot.rotation.x += 0.01;
+	mallaRobot.rotation.y += 0.01;
+
 	renderer.render( escena, camara );
 }
 
 var mallaRobot, camara, escena, renderer;
-var mallaRueda1, mallaRueda2, mallaRueda3, mallaRueda4, mallaVisor;
 
 setup();
 loop();
