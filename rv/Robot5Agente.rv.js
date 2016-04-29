33fe427d8248e99e4b9e497155ebe5e377ebe8dc
viktronic <pvictor3@hotmail.es>
2016-04-29 14:12:30 -0500

    Create Robot5Agente.rv.js

---
+++ rv/Robot5Agente.rv.js
@@ -0,0 +1,38 @@
+function Agent(x=0, y=0){
+ THREE.Object3D.call(this);   //Llama a cada objeto para que sean independientes
+ this.position.x=x;
+ this.position.y=y;
+}
+
+Agent.prototype = new THREE.Object3D();
+
+Agent.prototype.sense = function(environment) {};
+Agent.prototype.plan = function(environment) {};
+Agent.prototype.act = function(environment) {};
+
+function Environment() {
+ THREE.Scene.call(this);
+}
+
+Environment.prototype = new THREE.Scene();
+
+Environment.prototype.sense = function() {
+ for(var i=0; i<this.children.length; i++){
+  if(this.children[i].sense!==undefined)
+   this.children[i].sense(this);
+ }
+}
+
+Environment.prototype.plan = function() {
+ for(var i=0; i<this.children.length; i++){
+  if(this.children[i].plan!==undefined)
+   this.children[i].plan(this);
+ }
+}
+
+Environment.prototype.act = function() {
+ for(var i=0; i<this.children.length; i++){
+  if(this.children[i].act!==undefined)
+   this.children[i].act(this);
+ }
+}
